51605a27dc4614b72a55704f85397bf17def5c7c
marwa <mds08@mail.aub.edu>
2022-09-30 19:06:40 +0300

    done

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,6 @@
-let state = { count: 0 };
+let state;
 
-function changeState(state, action) {
+function changeState(state={count:0}, action) {
   switch (action.type) {
     case "counter/increment":
       return { count: state.count + 1 };
@@ -15,6 +15,7 @@ function dispatch(action) {
 }
 
 function render() {
-  const app = document.querySelector("#app");
-  app.textContent = state.count;
+  document.body.textContent=state.count;
 }
+
+dispatch({type:"@@INIT"});
